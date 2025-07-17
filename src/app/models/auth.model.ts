d48d34579957a59a2c192d667c49d4a85d8bfdc1
lamdev3107 <lamtx.3107@gmail.com>
2025-07-17 11:22:57 +0700

    first commit

---
+++ src/app/models/auth.model.ts
@@ -0,0 +1,7 @@
+export interface LoginResquest {
+  email: string;
+  password: string;
+}
+export interface LoginResponse {
+  accessToken: string;
+}
---
+++ src/app/models/user.model.ts
@@ -0,0 +1,19 @@
+export interface User {
+  id?: number;
+  email: string;
+  first_name: string;
+  last_name: string;
+  avatar?: string;
+}
+
+export interface UserResponse {
+  data: User[];
+  page: number;
+  per_page: number;
+  total: number;
+  total_pages: number;
+}
+
+export interface SingleUserResponse {
+  data: User;
+}
